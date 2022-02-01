f6beac5484c681da2c1a06e9e489add04336393d
Murat Karasah <mkarasah@gmail.com>
2022-02-01 14:15:59 +0300

    v1.2

--- sinavUygulamasi/Controllers/ContactController.cs
+++ sinavUygulamasi/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using sinavUygulamasi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,21 @@ namespace sinavUygulamasi.Controllers
 {
     public class ContactController : Controller
     {
+        sinavdbEntities1 db = new sinavdbEntities1();
+
         // GET: Contact
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(iletisim pm)
+        {
+
+            db.Entry(pm).State = System.Data.Entity.EntityState.Added;
+            db.SaveChanges();
+            return RedirectToAction("Index","Home");
+
         }
     }
 }
---
+++ sinavUygulamasi/Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sinavUygulamasi.Models
+{
+    public class ContactModel
+    {
+        public string Ad { get; set; }
+        public string Email { get; set; }
+        public string Mesaj { get; set; }
+
+
+
+    }
+}
